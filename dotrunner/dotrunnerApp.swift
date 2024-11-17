b5febebeb325832124f5cd099d4e59a9e1d8da40
swami-marsa <swami.marsa@gmail.com>
2024-11-17 07:26:20 +0400

    Initial Commit

---
+++ dotrunner/dotrunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotrunnerApp.swift
+//  dotrunner
+//
+//  Created by Swaminathan  on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct dotrunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
